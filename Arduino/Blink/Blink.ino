0d73fe0803bdc7613e0f41de1f170ee7fbcaba1f
Ho Yun "Bobby <ho.chan@colorado.edu>
2018-05-17 15:42:22 -0600

    Create Blink.ino

---
+++ Arduino/Blink/Blink.ino
@@ -0,0 +1,13 @@
+int ledPin =  13;
+
+void setup()   {                
+  pinMode(ledPin, OUTPUT);     
+}
+
+void loop()                     
+{
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
